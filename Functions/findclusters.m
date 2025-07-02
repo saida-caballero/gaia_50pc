2fcafbcd31b15a27058a1f7c2ec2fb84053f89d5
Rhiannon Hicks <rh415575@ucf.edu>
2025-07-02 11:48:57 -0400

    Update findclusters.m

--- Functions/findclusters.m
+++ Functions/findclusters.m
@@ -10,7 +10,7 @@ Example function call:
 
     findclusters(data{:,'RA (degrees)'}, data{:,'DEC (degrees)'}, data{:,'r_hi_geo (pc)'}, 'Example Plot')
 
-            - data for this example can be found here: https://github.com/saida-caballero/gaia_50pc
+            - data for this example can be found here: https://github.com/saida-caballero/gaia_50pc/tree/main/Data
 
 
 Input(s):
